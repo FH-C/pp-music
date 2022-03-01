cd1ee816ddc3e3feb9652350565090ad1a6208ba
FH <864383740@qq.com>
2022-03-01 18:36:14 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import styleImport, { VantResolve } from 'vite-plugin-style-import';
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
+import  {  VitePWA  }  from  'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     styleImport({
       resolves: [VantResolve()],
     }),
-  ]
+    VitePWA({}),
+  ],
 })
