69e77e0d664906f27d57afe94647112f2d8fe726
CYY <864383740@qq.com>
2022-03-03 00:15:09 +0800

    login

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,36 +1,38 @@
 <template>
   <div>
-    <div>
-      <img
-        alt="Vue logo"
-        src="../../assets/logo.png"
-        height="82"
-        width="82"
-      />
-    </div>
-    <div class="box1">
-      <div>
+    <div class="grid bg-red box">
+      <div class="logo">
+        <img
+          alt="Vue logo"
+          src="../../assets/logo.png"
+          height="82"
+          width="82"
+        />
+      </div>
+      <div class="button1">
         <Button
           round
           type="primary"
           plain
           hairline
           color="#db2c1f"
-          style="font-"
+          class="login-button"
+          @click="mobileLogin"
         >
           手机号登录
         </Button>
       </div>
-      <div>
+      <div class="button2">
         <Button
           round
           type="primary"
           color="#db2c1f"
+          class="login-button"
         >
           立即体验
         </Button>
       </div>
-      <div>
+      <div class="agree">
         <van-checkbox
           v-model="checked"
           class="agree"
@@ -59,12 +61,42 @@
 </template>
 
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
 import { Button } from 'vant'
 import { Checkbox } from 'vant'
 import { ref } from 'vue'
 
+const router = useRouter()
 const checked = ref(false)
+const mobileLogin = function () {
+  router.push('mobile')
+}
 </script>
 
 <style scoped lang="scss">
+@import url('../../styles/common.scss');
+
+.box {
+  grid-template-rows: repeat(20, 5%);
+  justify-items: center;
+}
+
+.logo {
+  grid-row-start: 6;
+}
+
+.button1 {
+  grid-row-start: 15;
+  justify-self: stretch;
+}
+
+.button2 {
+  grid-row-start: 17;
+  justify-self: stretch;
+}
+
+.agree {
+  grid-row-start: 19;
+  font-size: 12px;
+}
 </style>
